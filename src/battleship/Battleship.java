607f9ae7b084adf948bbc0e39284c7f49ccd3393
admin <danyalfayazi7@gamil.com>
2019-06-30 00:05:33 +0430

    Initial commit

---
+++ src/battleship/Battleship.java
@@ -0,0 +1,29 @@
+package battleship;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Battleship extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("battleship.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Battleship");
+        stage.setResizable(false);
+        stage.show();
+
+    }
+}
